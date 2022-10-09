6b1e82db5112442f65f1f3a77b5bf4c34056c97d
Lukas Lazarek <LukasALazarek@gmail.com>
2022-10-09 10:38:36 -0500

    Make mutate meta-package imply lib and doc packages

--- mutate/info.rkt
+++ mutate/info.rkt
@@ -3,6 +3,8 @@
 (define deps '("base"
                "mutate-lib"
                "mutate-doc"))
+(define implies '("mutate-lib"
+                  "mutate-doc"))
 (define build-deps '())
 (define pkg-desc "Program mutation library")
 (define pkg-authors '(lukas))
