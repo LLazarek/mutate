85eb2a904c02e730dc5c513409bfe2925759497f
Lukas Lazarek <LukasALazarek@gmail.com>
2023-06-30 12:05:03 -0500

    Add mutators pkg to meta-pkg

--- mutate/info.rkt
+++ mutate/info.rkt
@@ -2,6 +2,7 @@
 (define collection "mutate")
 (define deps '("base"
                "mutate-lib"
+               "mutate-mutators"
                "mutate-doc"))
 (define implies '("mutate-lib"
                   "mutate-doc"))
